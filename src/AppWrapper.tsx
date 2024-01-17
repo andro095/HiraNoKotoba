3d7185018e61c09b998fbb824c031f7de46558a2
andro095 <andre.rodriguezgt@yahoo.com>
2024-01-17 11:33:38 -0600

    Add favicon, app icons and path alias

---
+++ src/AppWrapper.tsx
@@ -0,0 +1,9 @@
+
+
+export const AppWrapper = () => {
+    return (
+        <div>
+            AppWrapper
+        </div>
+    )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@helpers': path.resolve(__dirname, './src/helpers'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@locales': path.resolve(__dirname, './src/locales'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    }
+  },
   plugins: [react()],
 })
